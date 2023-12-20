5ddda207248c5f963667f57bb00872590f8f6e3e
JamesJang-2jz <jhjx92@gmail.com>
2023-12-20 11:53:07 -0500

    ToDo entity

---
+++ my-todo-list-api/src/main/java/com/jamesjang/mytodolistapi/model/ToDo.java
@@ -0,0 +1,21 @@
+package com.jamesjang.mytodolistapi.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class ToDo {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String desc;
+    private boolean completed;
+    
+
+}
