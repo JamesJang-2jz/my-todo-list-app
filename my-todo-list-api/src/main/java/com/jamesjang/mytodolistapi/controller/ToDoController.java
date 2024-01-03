596e478e6bc111130864cd142db2116cf20fbeca
JamesJang-2jz <jhjx92@gmail.com>
2024-01-03 15:21:11 -0500

    added ToDo Controller

--- my-todo-list-api/src/main/java/com/jamesjang/mytodolistapi/controller/ToDoController.java
+++ my-todo-list-api/src/main/java/com/jamesjang/mytodolistapi/controller/ToDoController.java
@@ -1,4 +1,28 @@
 package com.jamesjang.mytodolistapi.controller;
 
+import com.jamesjang.mytodolistapi.model.ToDo;
+import com.jamesjang.mytodolistapi.service.ToDoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/todos")
+@CrossOrigin("http://localhost:3000")
 public class ToDoController {
+
+    private final ToDoService toDoService;
+
+    @GetMapping
+    public List<ToDo> getToDos() {
+        return toDoService.getToDos();
+    }
+
+    
+
 }
